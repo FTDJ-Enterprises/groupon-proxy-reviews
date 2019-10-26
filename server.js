2d0c4397d7d940bbf259c43a2fda8698e766967d
Jeehyae dance <jeehyae91@gmail.com>
2019-10-26 15:38:52 -0700

    added end points for bundles to request from each service

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const app = express()
+const compression = require ('compression');
 const axios = require('axios')
 const path = require('path')
 const port = 3000
 
+app.use(compression())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get("/api/:id/reviews", (req, res) => {
-    console.log('here')
     axios.get(`http://ec2-34-217-89-110.us-west-2.compute.amazonaws.com:3001/api/${req.params.id}/reviews`)
     .then (response => {
         res.send(response.data)
     })
     .catch(error => {
-        console.log(error)
+        console.log('review error')
+
     })
 })
 
@@ -23,9 +25,44 @@ app.get("/api/images/:productId", (req, res) => {
         res.send(response.data)
     })
     .catch(error => {
-        console.log(error)
+        console.log('slide show error')
+  
     })
 })
+
+app.get("/reviews/main.bundle.js", (req, res) => {
+    axios.get("http://ec2-34-217-89-110.us-west-2.compute.amazonaws.com:3001/main.bundle.js")
+    .then (response => {
+        res.send(response.data)
+    })
+    .catch(error => {
+        console.log(error)
+  
+    })
+})
+
+app.get("/slide-show/vendor.bundle.js", (req, res) => {
+    axios.get("http://ec2-18-236-153-219.us-west-2.compute.amazonaws.com:3002/vendor.bundle.js")
+    .then (response => {
+        res.send(response.data)
+    })
+    .catch(error => {
+        console.log(error)
+  
+    })
+})
+
+app.get("/slide-show/main.bundle.js", (req, res) => {
+    axios.get("http://ec2-18-236-153-219.us-west-2.compute.amazonaws.com:3002/main.bundle.js")
+    .then (response => {
+        res.send(response.data)
+    })
+    .catch(error => {
+        console.log(error)
+  
+    })
+})
+
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
